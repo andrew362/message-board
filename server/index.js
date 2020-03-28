92b014244ec2640dbe96a23bbcf25eb873f80593
Andrzej <andrzej362@gmail.com>
2020-03-29 00:02:14 +0100

    messages api

--- server/index.js
+++ server/index.js
@@ -15,6 +15,16 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users/:id/messages', loginRequired, ensureCorrectUser, messagesRoutes);
+app.use('/api/messages', loginRequired, async function(req,res,next){
+    try {
+        let messages = await db.Message.find().sort({createAt: 'desc'}).populate('user', {
+            username: true, profileImageUrl: true
+        });
+        return res.status(200).json(messages);
+    } catch (err) {
+        return next(err);        
+    }
+});
 
 app.use(function(req,res,next) {
     let err = new Error('Not Found!!');
--- server/models/message.js
+++ server/models/message.js
@@ -1,29 +1,34 @@
-const mongoose = require("mongoose");
-const User = require("./user");
+const mongoose = require('mongoose');
+const User = require('./user');
 
-const messageSchema = mongoose.Schema({
+const messageSchema = mongoose.Schema(
+  {
     text: {
-        type: String,
-        require: true,
-        maxLength: 160
+      type: String,
+      require: true,
+      maxLength: 160
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     }
+  },
+  {
+    timestamps: true
+  }
+);
+
+messageSchema.pre('remove', async function(next) {
+  try {
+    let user = await User.findById(this.user);
+    user.message.remove(this.id);
+    await user.save();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
-messageSchema.pre('remove', async function(next){
-    try {
-        let user = await User.findById(this.user);
-        user.message.remove(this.id);
-        await user.save();
-        return next();
-    } catch (err) {
-        return next(err);
-    }
-});
-
-const Message = mongoose.model("Message", messageSchema);
+const Message = mongoose.model('Message', messageSchema);
 
 module.exports = Message;
