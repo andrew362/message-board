a1c65b32ecd5f799e2f461f77fbfd308cfbee889
Andrzej <andrzej362@gmail.com>
2020-03-29 20:46:55 +0200

    redux store setup

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from '../store';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        Hello!
+      </div>
+    </Router>
+  </Provider>
+);
 
 export default App;
---
+++ client/src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
+export const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f)
+);
